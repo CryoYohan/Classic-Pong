a89d49feff1e22f75c0591d475e7a9a559b149b2
itsmecyril <cyrilypil@gmail.com>
2024-12-28 14:44:35 +0800

    shorter and cleaner version of initializing paddles

--- paddle.py
+++ paddle.py
@@ -1,51 +1,21 @@
 from turtle import Turtle
 class Paddle:
-    def __init__(self):
-        # Initialize Variables
-        self.WIDTH = 20
-        self.HEIGHT = 100
-        self.LEFT_XCOR = -350
-        self.RIGHT_XCOR = 350
-        self.YCOR = 0
-        # Initialize left and right turtle paddles
-        self.left = Turtle()
-        self.right = Turtle()
-        self.left.shape('square')
-        self.right.shape('square')
-        self.left_paddle()
-        self.right_paddle()
+    def __init__(self, coordinates):
+        # Initialize Paddle
+        self.paddle = Turtle()
+        self.paddle.penup()
+        self.paddle.shape('square')
+        self.paddle.color('white')
+        self.paddle.goto(coordinates)
+        self.paddle.shapesize(5, 1)
 
-    # Create the two paddles, left and right
+    # Control keys for the paddle
+    def go_up(self):
+        new_y = self.paddle.ycor() + 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
 
-    def right_paddle(self):
-        self.right.penup()
-        self.right.setx(self.RIGHT_XCOR)
-        self.right.sety(self.YCOR)
-        self.right.shapesize(5, 1)
-        self.right.color('white')
+    def go_down(self):
+        new_y = self.paddle.ycor() - 20
+        self.paddle.goto(self.paddle.xcor(), new_y)
 
-    def left_paddle(self):
-        self.left.penup()
-        self.left.setx(self.LEFT_XCOR)
-        self.left.sety(self.YCOR)
-        self.left.shapesize(5, 1)
-        self.left.color('white')
-
-    # Control keys for the left paddles
-    def go_up_left(self):
-        new_y = self.left.ycor() + 20
-        self.left.goto(self.left.xcor(), new_y)
-
-    def go_down_left(self):
-        new_y = self.left.ycor() - 20
-        self.left.goto(self.left.xcor(), new_y)
-
-    # Control keys for the right paddles
-    def go_up_right(self):
-        new_y = self.right.ycor() + 20
-        self.right.goto(self.right.xcor(), new_y)
-
-    def go_down_right(self):
-        new_y = self.right.ycor() - 20
-        self.right.goto(self.right.xcor(), new_y)
 
--- window.py
+++ window.py
@@ -4,24 +4,35 @@ from scoreboard import Scoreboard
 class Window(Turtle):
     def __init__(self):
         super().__init__()
-        # Initialize modules
-        self.paddles = Paddle()
+        # Initialize left and right paddles
+        self.paddles_left = Paddle((-350,0))
+        self.paddles_right = Paddle((350,0))
+
+        # Initialize Scoreboard
         self.scoreboard = Scoreboard()
-        self.window = Screen()
+
         # Setup window
+        self.window = Screen()
         self.window.colormode(255)
         self.window.setup(800,600)
         self.window.bgcolor('black')
         self.window.title('Pong')
+        self.window.tracer(0)
 
         # Listen Keys
         self.window.listen()
+        # Left Paddle Controls
+        self.window.onkeypress(self.paddles_left.go_up, 'w')
+        self.window.onkeypress(self.paddles_left.go_down, 's')
+        # Right Paddle Controls
+        self.window.onkeypress(self.paddles_right.go_up, 'Up')
+        self.window.onkeypress(self.paddles_right.go_down, 'Down')
 
-        self.window.onkey(self.paddles.go_up_left, 'w')
-        self.window.onkey(self.paddles.go_down_left, 's')
-
-        self.window.onkey(self.paddles.go_up_right, 'Up')
-        self.window.onkey(self.paddles.go_down_right, 'Down')
+        # Update Screen while game is running
+        self.game_running = True
+        while self.game_running:
+            self.window.update()
 
         self.window.exitonclick()
         self.window.mainloop()
+
